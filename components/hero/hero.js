913527f6c7abdcda3aed190f94dc0929829ba82d
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-10-18 22:12:44 -0600

    adjusted responsive styles >1440px

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -12,35 +12,37 @@ function Hero() {
 
 	return (
 		<section className={styles.hero}>
-			{/* Ellipses background image */}
-			<div className={styles.hero__ellipses}></div>
+			<div className={styles.hero__page_container}>
+				{/* Ellipses background image */}
+				<div className={styles.hero__ellipses}></div>
 
-			{/* Hero content block */}
-			<div className={styles.hero__block}>
-				<h1 className={styles.hero__title}>
-					We help businesses <span>expand</span> their{' '}
-					<span>digital presence</span>
-				</h1>
-				<p className={styles.hero__content}>
-					We aide small businesses in growing their online presence by providing
-					engaging website design & development and SEO strategies.
-				</p>
-				<Button
-					type='button'
-					className={styles.hero__button}
-					onClick={handleBtnClick}
-					text='learn more'
-				/>
-			</div>
+				{/* Hero content block */}
+				<div className={styles.hero__block}>
+					<h1 className={styles.hero__title}>
+						We help businesses <span>expand</span> their{' '}
+						<span>digital presence</span>
+					</h1>
+					<p className={styles.hero__content}>
+						We aide small businesses in growing their online presence by
+						providing engaging website design & development and SEO strategies.
+					</p>
+					<Button
+						type='button'
+						className={styles.hero__button}
+						onClick={handleBtnClick}
+						text='learn more'
+					/>
+				</div>
 
-			{/* Hero Image */}
-			<div className={styles.hero__imageWrapper}>
-				<Image
-					src={heroImg}
-					alt='consult'
-					className={styles.hero__image}
-					layout='fill'
-				/>
+				{/* Hero Image */}
+				<div className={styles.hero__imageWrapper}>
+					<Image
+						src={heroImg}
+						alt='consult'
+						className={styles.hero__image}
+						layout='fill'
+					/>
+				</div>
 			</div>
 		</section>
 	);
