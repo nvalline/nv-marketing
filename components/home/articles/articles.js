34c8f7f2d41c2436d4d1c8e1001c9e3a8fafc522
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-10-26 22:12:36 -0600

    added basic articles & getStaticProps

--- components/home/articles/articles.js
+++ components/home/articles/articles.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Button from '../../misc/Buttons';
 
-import articleImg from '../../../public/images/posts/devices.jpg';
+import articleImg from '../../../public/images/articles/devices.jpg';
 
 import styles from '../../../styles/home/articles/ArticleSection.module.scss';
 
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import { promises as fs } from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-function Index() {
-	return <div>Blog List Page</div>;
+import styles from '../../styles/articles/Index.module.scss';
+
+export default function Index({ articles }) {
+	return (
+		<section className={styles.articles}>
+			<div className={styles.articles__container}>
+				{/* Ellipses background image */}
+				<div className={styles.articles__ellipses}></div>
+
+				{/* Article Preview */}
+				<div>
+					{articles.map((article) => {
+						return <h3>{article.frontmatter.title}</h3>;
+					})}
+				</div>
+			</div>
+		</section>
+	);
 }
 
-export default Index;
+export async function getStaticProps() {
+	// Get all article filenames
+	const articlesDirectory = path.join(process.cwd(), '/data/articles');
+	const filenames = await fs.readdir(articlesDirectory);
+
+	// Get slug and frontmatter from articles
+	const articles = filenames.map(async (filename) => {
+		// Create slug
+		const slug = filename.replace('.md', '');
+
+		// Get front matter
+		const filePath = path.join(articlesDirectory, filename);
+		const fileContent = await fs.readFile(filePath, 'utf8');
+
+		const { data: frontmatter } = matter(fileContent);
+
+		return {
+			slug,
+			frontmatter
+		};
+	});
+
+	return {
+		props: {
+			articles: await Promise.all(articles)
+		}
+	};
+}
