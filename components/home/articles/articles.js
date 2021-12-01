d2a37fbfd7f4629d0d05e26a7654fb6d3611bc7c
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-12-01 15:03:49 -0700

    updated homepage articles content & image

--- components/home/articles/articles.js
+++ components/home/articles/articles.js
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import Button from '../../misc/Buttons';
 
-import articleImg from '../../../public/images/articles/devices.jpg';
+import articleImg from '../../../public/images/articles/digital_world.png';
 
 import styles from '../../../styles/home/articles/ArticleSection.module.scss';
 
@@ -26,19 +26,29 @@ function Articles() {
 				</div>
 				<div className={styles.articles__contentWrapper}>
 					<h2 className={styles.articles__title}>
-						Tips To Expand Your Digital Presence
+						The Importance of Digital Presence for Business
 					</h2>
 					<p className={styles.articles__desc}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sapien
-						rhoncus, lobortis neque pellentesque. Maecenas erat turpis sapien
-						semper nulla risus. Venenatis cras sed quam sagittis, iaculis mauris
-						in egestas.
+						Having a digital presence for your business is vital to its success.
+						Your business needs a strong presence to attract new customers and
+						keep current ones.
+					</p>
+					<p className={styles.articles__desc}>
+						A digital presence will help market your company's brand. It will
+						improve the ease of showcasing products and services. Customers will
+						be able to easily find you. And you will be able to build lasting
+						relationships with your customers.
+					</p>
+					<p className={styles.articles__desc}>
+						There are several ways to build a digital presence and it won't
+						happen overnight. It is best to put a plan in place, stick to it and
+						set attainable goals.
 					</p>
 					<Link href='/articles' passHref>
 						<Button
 							type='button'
 							className={styles.articles__button}
-							text='read more'
+							text='explore more'
 						/>
 					</Link>
 				</div>
