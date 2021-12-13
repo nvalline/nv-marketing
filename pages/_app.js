f3e24be545823113ec25cb007bf32c6edfcb1026
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-12-12 18:35:16 -0700

    added GA & adjusted styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 import '../styles/globals.scss';
 
@@ -11,6 +12,20 @@ export default function MyApp({ Component, pageProps }) {
 				<meta charSet='utf-8' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
+			<Script
+				strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<Script strategy='lazyOnload'>
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+			</Script>
 			{getLayout(<Component {...pageProps} />)}
 		</>
 	);
