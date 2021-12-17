3cde2de99b450ed8a348abdf47879ba335c618d5
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-12-17 10:27:56 -0700

    added open graph to home

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,14 @@ import { projects } from '../data/Projects';
 import { services } from '../data/Services';
 import { testimonials } from '../data/Testimonials';
 
-export default function Home({ projectInfo, serviceInfo, testimonialInfo }) {
+import og_img_file from '../git_img/og_website_image_fb.png';
+
+export default function Home({
+	og_img,
+	projectInfo,
+	serviceInfo,
+	testimonialInfo
+}) {
 	return (
 		<>
 			<Head>
@@ -27,6 +34,11 @@ export default function Home({ projectInfo, serviceInfo, testimonialInfo }) {
 					name='keywords'
 					content='website design, website development, seo, digital presence, design agency, freelance, web builder, small business website'
 				/>
+				{/* Open Graph Protocol */}
+				<meta property='og:title' content='NV Marketing, LLC' />
+				<meta property='og:type' content='website' />
+				<meta property='og:image' content={og_img} />
+				<meta property='og:url' content='https://nv-marketing.com' />
 			</Head>
 
 			{/* Main Hero Section */}
@@ -57,6 +69,7 @@ Home.getLayout = function getLayout(page) {
 export async function getStaticProps() {
 	return {
 		props: {
+			og_img: og_img_file,
 			projectInfo: projects,
 			serviceInfo: services,
 			testimonialInfo: testimonials
