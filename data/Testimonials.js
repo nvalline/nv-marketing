0aff63bd3715f087cc67fd58ab5d3f3fe27c6f35
vall4251n <58278138+nvalline@users.noreply.github.com>
2022-09-14 10:22:05 -0600

    updated testimonials

--- data/Testimonials.js
+++ data/Testimonials.js
@@ -1,26 +1,26 @@
 export const testimonials = [
-	{
-		id: 1,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus, atque assumenda aut non facilis tempore provident! Possimus, placeat fugit?',
-		name: 'Joe Dirt',
-		position: 'Owner',
-		company: 'Hemi Industries, LLC'
-	},
-	{
-		id: 2,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus',
-		name: 'Ricky Bobby',
-		position: 'Owner/Driver',
-		company: 'ME Racing Team'
-	},
-	{
-		id: 3,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil',
-		name: 'Jane Smith',
-		position: 'Owner',
-		company: 'Scoped Optics'
-	}
+	// {
+	// 	id: 1,
+	// 	content:
+	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus, atque assumenda aut non facilis tempore provident! Possimus, placeat fugit?',
+	// 	name: 'Joe Dirt',
+	// 	position: 'Owner',
+	// 	company: 'Hemi Industries, LLC'
+	// },
+	// {
+	// 	id: 2,
+	// 	content:
+	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus',
+	// 	name: 'Ricky Bobby',
+	// 	position: 'Owner/Driver',
+	// 	company: 'ME Racing Team'
+	// },
+	// {
+	// 	id: 3,
+	// 	content:
+	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil',
+	// 	name: 'Jane Smith',
+	// 	position: 'Owner',
+	// 	company: 'Scoped Optics'
+	// }
 ];
