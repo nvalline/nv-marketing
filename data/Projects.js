7c56e35bcc66e8669cecad98bd43b7a5624f9562
vall4251n <58278138+nvalline@users.noreply.github.com>
2022-09-14 10:17:54 -0600

    added HMHP project to site

--- data/Projects.js
+++ data/Projects.js
@@ -1,17 +1,17 @@
 export const projects = [
-	// {
-	// 	id: 1,
-	// 	img: '/images/projects/high-mtn-hp.png',
-	// 	img_blur:
-	// 		'|+H{i^%2WBt8WBaeaybHae_Ns:WBjZWBayaej[j[%MoekCoej]j[ayfQfkoeV@WBWVjsj[j[j[jtM{azfkaxoefkayayayNGjtayj[j@a|f6j[j@ogt7j[fQa{ayayayfQayWBayayayayfkj[j[oLayj[oLayj[j[j[ay',
-	// 	name: 'High Mountain Home Professionals',
-	// 	description:
-	// 		'This website for High Mountain Home Professionals was created with Next.js and Sanity.io.  This site was custom designed to meet the business owner's goal of increasing their interent presence while also generating more leads.',
-	// 	url: 'https://highmnthp.com',
-	// 	accent: 'right'
-	// },
 	{
 		id: 1,
+		img: '/images/projects/high-mtn-hp.png',
+		img_blur:
+			'|+H{i^%2WBt8WBaeaybHae_Ns:WBjZWBayaej[j[%MoekCoej]j[ayfQfkoeV@WBWVjsj[j[j[jtM{azfkaxoefkayayayNGjtayj[j@a|f6j[j@ogt7j[fQa{ayayayfQayWBayayayayfkj[j[oLayj[oLayj[j[j[ay',
+		name: 'High Mountain Home Professionals',
+		description:
+			"This website for High Mountain Home Professionals was created with Next.js and Sanity.io.  This site was custom designed to meet the business owner's goal of increasing their interent presence while also generating more leads.",
+		url: 'https://highmnthp.com',
+		accent: 'left'
+	},
+	{
+		id: 2,
 		img: '/images/projects/nova-art-restoration.jpeg',
 		img_blur:
 			'|IHU2ox^02IUEhxt%LoejE0Vx[~Txtw]V[NHWBW=NeR*IpxutRWCn%jZsoMyEMxDRQR-ogbIt6X8V[M|M|t5W=ofe:RjjF%2%1IoM{smW=R+SeWVj]t7xZWBnhnhoJofSNbHWX%1t6RjRjo0xZofSNW;WBocbbt6WCV[xF',
@@ -19,10 +19,10 @@ export const projects = [
 		description:
 			'Nova Art Restoration is a custom website for an art restoration company.  Created with Next.js and Sanity.is, this is a fast and responsive site with the objective to create more leads for art restoration services.',
 		url: 'https://www.novaartrestoration.com',
-		accent: 'left'
+		accent: 'right'
 	},
 	{
-		id: 2,
+		id: 3,
 		img: '/images/projects/nv-portfolio.jpg',
 		img_blur:
 			'|NEMOMa|bGj[fQj[fkj[ay00ayayayfQa|fQayfQ~qj[ayfQfQj[aya|j[ITazj[fQfQfQfQfQfQj[j[j[ayfQa|j[j[fQoLj[a|fQfkjtfQa|fQofayf6j[fQa|fQfQfQWBa|fQayayayfQj[j@oej[aya{fQj[fQfQjt',
@@ -30,6 +30,6 @@ export const projects = [
 		description:
 			"This custom portfolio was designed to be an eye catching showcase of the owner's work and style.  Created with Next.js and Sanity.io, this is fully responsive for mobile.",
 		url: 'https://natevalline.com',
-		accent: 'right'
+		accent: 'left'
 	}
 ];
