453c73235961dfd3072270beb90743431c798774
vall4251n <58278138+nvalline@users.noreply.github.com>
2022-09-28 18:41:54 -0600

    added testimonial and adjusted styles

--- data/Testimonials.js
+++ data/Testimonials.js
@@ -1,16 +1,16 @@
 export const testimonials = [
-	// {
-	// 	id: 1,
-	// 	content:
-	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus, atque assumenda aut non facilis tempore provident! Possimus, placeat fugit?',
-	// 	name: 'Joe Dirt',
-	// 	position: 'Owner',
-	// 	company: 'Hemi Industries, LLC'
-	// },
+	{
+		id: 1,
+		content:
+			"I had Nate build me a website for my painting and drywall business. He did a fantastic job creating me a website that reflects my vision for what I wanted. He helped me set up a lot of extra tools for my website and walked me through how to use them. He was very professional and had great communication throughout the whole process which was very appreciated. I will definitely continue to use Nate's services in the future for any website or marketing needs I have in the future.",
+		name: 'Connor Z.',
+		position: 'Owner',
+		company: 'High Mountain Home Professionals'
+	}
 	// {
 	// 	id: 2,
 	// 	content:
-	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus',
+	// 		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil, eum alias rem autem expedita veniam explicabo sed est corporis nulla aliquam dolore aliquid laudantium similique accusamus",
 	// 	name: 'Ricky Bobby',
 	// 	position: 'Owner/Driver',
 	// 	company: 'ME Racing Team'
@@ -18,7 +18,7 @@ export const testimonials = [
 	// {
 	// 	id: 3,
 	// 	content:
-	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil',
+	// 		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quas iure quod nihil",
 	// 	name: 'Jane Smith',
 	// 	position: 'Owner',
 	// 	company: 'Scoped Optics'
