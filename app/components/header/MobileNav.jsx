7d8926bc0b251e89debb2814c31c1e469a878e1a
vall4251n <58278138+nvalline@users.noreply.github.com>
2023-09-08 14:22:16 -0600

    #34 - should add mobile header and nav

--- app/components/header/MobileNav.jsx
+++ app/components/header/MobileNav.jsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 // Components
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
@@ -10,22 +11,64 @@ import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import styles from '@/app/styles/components/header/MobileNav.module.scss';
 
 export default function MobileNav() {
+	const pathname = usePathname();
 	const [showIcon, setShowIcon] = useState(false);
 
 	const handleIconToggle = () => {
 		setShowIcon(!showIcon);
 	};
 
+	// Disable scroll when overlay is active
+	useEffect(() => {
+		if (showIcon) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'unset';
+		}
+	}, [showIcon]);
+
+	// Update icon state on route change
+	useEffect(() => {
+		setShowIcon(false);
+	}, [pathname]);
+
 	return (
-		<>
+		<div>
 			{/* OnClick Change Icon */}
 			<div onClick={handleIconToggle}>
-				{showIcon ? <AiOutlineClose /> : <AiOutlineMenu />}
+				{showIcon ? (
+					<AiOutlineClose size={20} className={styles.mobile_nav__icons} />
+				) : (
+					<AiOutlineMenu size={20} className={styles.mobile_nav__icons} />
+				)}
 			</div>
 
-			<div>
-				<p>menu items</p>
+			<div
+				className={
+					showIcon
+						? `${styles.mobile_nav__overlay} ${styles.mobile_nav__fadeIn}`
+						: `${styles.mobile_nav__overlay} ${styles.mobile_nav__fadeOut}`
+				}
+			>
+				<div className={styles.mobile_nav__links}>
+					<ul className={styles.mobile_nav__linksList}>
+						<li className={styles.mobile_nav__link}>
+							<Link href='/about'>About</Link>
+						</li>
+						<li className={styles.mobile_nav__link}>
+							<Link href='/services'>Services</Link>
+						</li>
+						<li className={styles.mobile_nav__link}>
+							<Link href='/blog'>Blog</Link>
+						</li>
+					</ul>
+					<Link href='/contact'>
+						<button type='button' className={styles.mobile_nav__button}>
+							Contact
+						</button>
+					</Link>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
