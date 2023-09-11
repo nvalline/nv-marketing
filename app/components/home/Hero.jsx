dbfaaa4292f5533455bfd9b048825d1c101e1630
vall4251n <58278138+nvalline@users.noreply.github.com>
2023-09-10 21:17:37 -0600

    #43 - should add mobile hero section styles

--- app/components/home/Hero.jsx
+++ app/components/home/Hero.jsx
@@ -4,26 +4,28 @@ import styles from '@/app/styles/components/home/Hero.module.scss';
 export default function Hero() {
 	return (
 		<section className={styles.hero}>
-			<div>
+			<div className={styles.hero__wrapper}>
 				{/* Content */}
-				<div>
-					<h1>
-						Unleash Your <span>Business’s Potential</span>
+				<div className={styles.hero__content}>
+					<h1 className={styles.hero__header}>
+						Unleash Your <span>Business&apos;s Potential</span>
 					</h1>
-					<p>
+					<p className={styles.hero__sub_header}>
 						Welcome to NV Marketing, where digital innovation meets
 						affordability. We&apos;re on a mission to{' '}
 						<span>empower small businesses</span> like yours with the online
 						presence of giants.{' '}
 					</p>
-					<p>
+					<p className={styles.hero__sub_header}>
 						Our expertise <span>transforms your vision</span> into a stunning
 						reality, all within the reach of your budget.
 					</p>
-					<button type='button'>Get In Touch</button>
+					<button type='button' className={styles.hero__btn}>
+						Get In Touch
+					</button>
 				</div>
 				{/* Image */}
-				<div></div>
+				<div className={styles.hero__img}></div>
 			</div>
 		</section>
 	);
