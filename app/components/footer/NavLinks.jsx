e48c39b63dd4c11a6559773b849dfca156ded1c3
vall4251n <58278138+nvalline@users.noreply.github.com>
2023-09-14 14:46:58 -0600

    #72 - Should correct the li links structure

--- app/components/footer/NavLinks.jsx
+++ app/components/footer/NavLinks.jsx
@@ -7,21 +7,21 @@ export default function NavLinks() {
 	return (
 		<>
 			<ul className={styles.nav_links}>
-				<Link href={'/'}>
-					<li>Home</li>
-				</Link>
-				<Link href={'/about'}>
-					<li>About</li>
-				</Link>
-				<Link href={'/services'}>
-					<li>Services</li>
-				</Link>
-				<Link href={'/blog'}>
-					<li>Blog</li>
-				</Link>
-				<Link href={'/contact'}>
-					<li>Contact</li>
-				</Link>
+				<li>
+					<Link href={'/'}>Home</Link>
+				</li>
+				<li>
+					<Link href={'/about'}>About</Link>
+				</li>
+				<li>
+					<Link href={'/services'}>Services</Link>
+				</li>
+				<li>
+					<Link href={'/blog'}>Blog</Link>
+				</li>
+				<li>
+					<Link href={'/contact'}>Contact</Link>
+				</li>
 			</ul>
 		</>
 	);
