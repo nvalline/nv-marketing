9dcedc7bbc963845967aba1dc9ca20caeb796086
vall4251n <58278138+nvalline@users.noreply.github.com>
2023-09-22 16:58:01 -0600

    #84 - Should add core values to Sanity

---
+++ sanity/schemas/documents/CoreValues.js
@@ -0,0 +1,14 @@
+// CoreValues.js
+
+export default {
+  name: 'coreValues',
+  type: 'document',
+  title: 'Core Values',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Value Title',
+    },
+  ],
+}
