38e2b7e5642b7a6b8da5aa848c8d9baeeae46edc
vall4251n <58278138+nvalline@users.noreply.github.com>
2023-09-23 09:56:36 -0600

    #86 - Should add commitments section to mobile about page

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,7 +1,8 @@
 import CallToActions from './documents/CallToActions'
+import Commitments from './documents/Commitments'
 import CoreValues from './documents/CoreValues'
 import Service from './objects/Service'
 import Services from './documents/Services'
 import Testimonials from './documents/Testimonials'
 
-export const schemaTypes = [CallToActions, CoreValues, Service, Services, Testimonials]
+export const schemaTypes = [CallToActions, Commitments, CoreValues, Service, Services, Testimonials]
