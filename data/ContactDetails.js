7831baffc2938132895ff5f8575db158c691e1aa
vall4251n <58278138+nvalline@users.noreply.github.com>
2023-10-26 11:15:24 -0600

    #174 - Should delete duplicate files

--- data/ContactDetails.js
+++
@@ -1,8 +0,0 @@
-export const contactDetails = [
-	{
-		id: 1,
-		name: 'NV Marketing, LLC',
-		phone: '+1 (801) 742-1290',
-		email: 'contact@nv-marketing.com'
-	}
-];
--- data/Og_data.js
+++
@@ -1,7 +0,0 @@
-export const og_data = [
-	{
-		id: 1,
-		name: 'website_img',
-		img_url: '/images/og_website_image_fb.png'
-	}
-];
