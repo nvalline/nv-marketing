81691994bcb909b82dbaab13ef2e05aaf9ab9ac7
vall4251n <58278138+nvalline@users.noreply.github.com>
2026-06-08 18:12:19 -0600

    feat: make AuthorBio accept dynamic author prop

--- app/components/blog/AuthorBio.jsx
+++ app/components/blog/AuthorBio.jsx
@@ -1,19 +1,17 @@
 import Link from 'next/link';
 import styles from '@/app/styles/components/blog/AuthorBio.module.scss';
 
-export default function AuthorBio() {
+export default function AuthorBio({ author }) {
 	return (
 		<div className={styles.author_bio}>
 			<div className={styles.author_bio__wrapper}>
-				<p className={styles.author_bio__name}>Nate Valline</p>
-				<p className={styles.author_bio__credentials}>
-					Web developer and digital marketing consultant with 10+ years of
-					experience helping small businesses grow their online presence through
-					strategy-driven websites and SEO.
-				</p>
-				<Link href='/about' className={styles.author_bio__link}>
-					About Nate
-				</Link>
+				<p className={styles.author_bio__name}>{author.name}</p>
+				<p className={styles.author_bio__credentials}>{author.bio}</p>
+				{author.url && (
+					<Link href={author.url} className={styles.author_bio__link}>
+						About {author.name}
+					</Link>
+				)}
 			</div>
 		</div>
 	);
